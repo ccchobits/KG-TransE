4f5e7964e49f2742ede3db37456af47175900d8b
Chen Chen PhD SPIRIT <s190009@scsegc.cm.cluster>
2020-09-23 03:57:35 +0800

    add run.sh first run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 margin=(1.0 2.0)
 dataset_name=WN18
 bern=(True False)
@@ -9,4 +11,4 @@ margin=1.0
 lr_decay=1.7
 norm=1
 
-chmod +x ./main/main.py
+python3 ../main/main.py --dim 64 --bs 2048 --init_lr 0.01 --lr_decay 1.7 --bern False --margin 1.0 --norm 1 
